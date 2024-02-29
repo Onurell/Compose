547b5e955b588efce6d67c7b9d31e375ca908f4e
ahmetonur.argenc <ahmetonur.argenc@dgpaysit.com>
2024-02-29 14:09:13 +0300

    kisilerUygulaması

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,16 +12,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,7 +49,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    SayfaGecisleri()
+                    Anasayfa()
                 }
             }
         }
@@ -59,53 +66,32 @@ fun DefaultPreview() {
 
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SayfaGecisleri() {
+fun Anasayfa() {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {Text(text = "Kişiler")})
+        },
+        content = {
 
-    val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "anasayfa"){
-        composable("anasayfa"){
-            Anasayfa(navController =  navController)
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { },
+                content = {
+                    Icon(
+                        painter = painterResource (id = R.drawable.ekle),
+                        contentDescription = "", tint = Color.White)
+                }
+
+            )
         }
-
-        composable("tahmin_ekrani"){
-            TahminEkrani(navController =  navController)
-        }
-
-        composable("sonuc_ekrani/{sonuc}",arguments = listOf(
-            navArgument("sonuc"){type = NavType.BoolType}
-        )){
-            val sonuc = it.arguments?.getBoolean("sonuc")!!
-            SonucEkrani(sonuc = sonuc, navController)
-        }
-    }
-
+    )
 }
 
 
 
-@Composable
-fun Anasayfa(navController: NavController) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement =  Arrangement.SpaceEvenly,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Tahmin Oyunu",
-            color = Color.Black,
-            fontSize = 36.sp)
-            Image(painter = painterResource(id = R.drawable.zar_resmi),
-                 contentDescription = "")
 
-        Button(onClick = {
-                         navController.navigate("tahmin_ekrani")
-        },modifier = Modifier.size(250.dp, 50.dp)) {
-            Text(text = "OYUNA BAŞLA")
-        }
-    }
-
-
-
-}
 
